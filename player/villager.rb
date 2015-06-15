4e2911210c38925e0acbfa7b1d69a35c04230fa8
Mahendhar <mahendarreddy@live.com>
2015-06-15 16:30:13 +0530

    Created cop role for Wolf game

---
+++ player/villager.rb
@@ -0,0 +1,24 @@
+require_relative 'player'
+
+class Villager < Player
+  def initialize(game)
+    super(game)
+    @role = Player::Role::SIMPLE_VILLAGER
+  end
+
+  def pick_victim_by_voting
+    if cop?
+      puts "Cop voted for #{$test}"
+      return cop_selects
+    else
+    return (@game.players.alive - [self]).sample
+    end
+
+  end
+
+protected
+  def name_prefix
+    "Villager_"
+  end
+
+end
